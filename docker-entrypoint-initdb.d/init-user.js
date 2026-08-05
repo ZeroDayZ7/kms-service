bc245ad40e985b76f515eb12374fb7db6ee573b0
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-08-05 12:50:52 +0200

    chore: remove disabled deployment workflow and update init-user script comments

--- docker-entrypoint-initdb.d/init-user.js
+++ docker-entrypoint-initdb.d/init-user.js
@@ -1,14 +1,11 @@
 // docker-entrypoint-initdb.d/init-user.js
 
-// Odczytujemy zmienne środowiskowe przekazane do kontenera przez Docker Compose
 const appUser = process.env.MONGO_APP_USER;
 const appPwd = process.env.MONGO_APP_PASSWORD;
 const appDb = process.env.MONGO_APP_DATABASE;
 
-// Przełączamy się na bazę aplikacji
 db = db.getSiblingDB(appDb);
 
-// Tworzymy dedykowanego użytkownika aplikacji
 db.createUser({
   user: appUser,
   pwd: appPwd,
